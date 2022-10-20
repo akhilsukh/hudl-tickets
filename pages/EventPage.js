cddad2bbea901d1905ab186f30677872cb81085d
Preethi Muthukumaran <preethi.m@berkeley.edu>
2022-10-19 21:40:39 -0700

    created basic page templates.

---
+++ pages/EventPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, SafeAreaView, ScrollView, View, Text } from 'react-native';
+
+export default function EventPage({ navigation }) {
+
+  return (
+    <View>
+        <Text>Event Page</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
---
+++ pages/ExplorePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, SafeAreaView, ScrollView, View, Text } from 'react-native';
+
+export default function ExplorePage({ navigation }) {
+
+  return (
+    <View>
+        <Text>Explore Page</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
