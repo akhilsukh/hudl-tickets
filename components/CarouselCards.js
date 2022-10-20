4b5210f4fe71a1bc48d9bfbffadb95e231b81466
Amal Deepak Thiru <adthiru@berkeley.edu>
2022-10-19 23:07:23 -0700

    currently working carousel, with added downloads

--- components/CarouselCards.js
+++ components/CarouselCards.js
@@ -1,6 +1,7 @@
 import { setStatusBarTranslucent } from 'expo-status-bar'
 import React from 'react'
 import { View } from "react-native"
+//import View from "deprecated-react-native-prop-types"
 import Carousel, { Pagination } from 'react-native-snap-carousel'
 import CarouselCardItem, { SLIDER_WIDTH, ITEM_WIDTH } from './CarouselCardItem'
 import data from './data'
@@ -46,3 +47,42 @@ export default function CarouselCards() {
     </View>
   )
 }
+
+
+// import * as React from 'react';
+// import { Dimensions, Text, View } from 'react-native';
+// import Carousel from 'react-native-reanimated-carousel';
+
+// function Index() {
+//     const width = Dimensions.get('window').width;
+//     return (
+//         <View style={{ flex: 1 }}>
+//             <Carousel
+//                 loop
+//                 width={width}
+//                 height={width / 2}
+//                 autoPlay={true}
+//                 data={[...new Array(6).keys()]}
+//                 scrollAnimationDuration={1000}
+//                 onSnapToItem={(index) => console.log('current index:', index)}
+//                 renderItem={({ index }) => (
+//                     <View
+//                         style={{
+//                             flex: 1,
+//                             borderWidth: 1,
+//                             justifyContent: 'center',
+//                         }}
+//                     >
+//                         <Text style={{ textAlign: 'center', fontSize: 30 }}>
+//                             {index}
+//                         </Text>
+//                     </View>
+//                 )}
+//             />
+//         </View>
+//     );
+// }
+
+// export default Index;
+
+
