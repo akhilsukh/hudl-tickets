a6ec71a9209e55a8cd74643978340e1bf537ddd8
Preethi Muthukumaran <preethi.m@berkeley.edu>
2022-10-20 23:06:25 -0700

    Created page templates for ExplorePage.js, EventPage.js, TicketsPage.js. Includes the banner and navigation between pages.

--- pages/EventPage.js
+++ pages/EventPage.js
@@ -1,15 +1,41 @@
 import React from 'react'
-import { StyleSheet, SafeAreaView, ScrollView, View, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, ScrollView, View, Text, Button, Image } from 'react-native';
+import Banner from '../images/banner.png'
 
 export default function EventPage({ navigation }) {
 
   return (
-    <View>
+    <View style = {styles.container}>
+      <View style = {styles.banner}>
+        <Image style = {styles.bannerImage} source = {Banner}/>
+      </View>
+      <View style = {styles.content}>
         <Text>Event Page</Text>
-    </View>
+        <Button
+        title = "Home" 
+        onPress={() => {navigation.navigate("Explore Page")}}
+        />
+      </View>
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  banner: {
+    flex: 1
+  }, 
+  bannerImage: {
+    width: 1300,
+    height: 100,
+    resizeMode: "cover"
+  }, 
+  content: {
+    flex: 2
+  }
 });
--- pages/ExplorePage.js
+++ pages/ExplorePage.js
@@ -1,15 +1,46 @@
 import React from 'react'
-import { StyleSheet, SafeAreaView, ScrollView, View, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, ScrollView, View, Text, Button, Image} from 'react-native';
+// import Banner from '../images/banner.png'
+import Banner from '../images/banner.png'
 
 export default function ExplorePage({ navigation }) {
 
   return (
-    <View>
+    <View style = {styles.container}>
+      <View style = {styles.banner}>
+        <Image style = {styles.bannerImage} source = {Banner}/>
+      </View>
+      <View style = {styles.content}>
         <Text>Explore Page</Text>
+        <Button
+        title = "Tickets" 
+        onPress={() => {navigation.navigate("Tickets Page")}}
+        />
+        <Button
+        title = "Event Page" 
+        onPress={() => {navigation.navigate("Event Page")}}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  banner: {
+    flex: 1
+  }, 
+  bannerImage: {
+    width: 1300,
+    height: 100,
+    resizeMode: "cover"
+  }, 
+  content: {
+    flex: 2
+  }
 });
