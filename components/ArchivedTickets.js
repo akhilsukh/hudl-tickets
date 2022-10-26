f8c5d55964074b34cb3a2da2417775622ccbb409
Sathvika <sathvika@berkeley.edu>
2022-10-25 23:33:34 -0700

    added jenna and vuk's shit

---
+++ components/ArchivedTickets.js
@@ -0,0 +1,44 @@
+import {StyleSheet, Text, View, Button} from "react-native";
+import {useState, React} from 'react';
+import Grid from "./Grid.js";
+
+
+export default function ArchivedTickets({data}) {
+    const [minButton, setButton] = useState("Archived Tickets");
+    const [minimized, setMinimized] = useState(true);
+
+    const clickMinimize = () => {
+        if (minButton == "Archived Tickets") {
+            setButton("Collapse");
+            setMinimized(false);
+        } else {
+            setButton("Archived Tickets");
+            setMinimized(true);
+        }
+    }
+    
+    return (
+        <View>
+            <Button title = {minButton} color = "#FCA974" width = "100px" onPress = {clickMinimize}></Button>
+        {/*dummyData.archivedTickets?.map( ticket => {*/ }
+            {
+                !minimized? 
+                    (<Grid data = {data} ></Grid>)
+                : []
+            }
+
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    grid: {
+      backgroundColor: "black",
+      flexDirection: "row",
+      flexWrap: "wrap",
+      alignItems: "flex-start",
+      justifyContent: "space-between",
+      padding: 5
+    }
+  });
