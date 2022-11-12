58c2290b8ff029a9d1c36a728d553fcddf6fdcfb
Sathvika <sathvika@berkeley.edu>
2022-11-12 04:16:40 -0800

    fixed category page

--- pages/CategoryPage.js
+++ pages/CategoryPage.js
@@ -1,28 +1,58 @@
-import React, { useEffect } from 'react'
+import React, {useState, useEffect } from 'react'
 import { StyleSheet, SafeAreaView, View, Text, ScrollView } from 'react-native';
 import data from '../components/data'
 import EventChip from '../components/EventChip'
+import { db } from '../firebaseConfig.js';
+//import { getDoc, doc } from 'firebase/firestore';
+import { collection, getDocs } from "firebase/firestore";
 
 export default function CategoryPage({route, navigation }) {
     //const category = navigation.state.params.category
     const { category } = route.params;
-    let filteredData = Object.entries(data)
-    filteredData = data.filter((item) => item.category == category)
+    console.log(category)
+    //let filteredData = Object.entries(data)
+    //filteredData = data.filter((item) => item.category == category)
 
-    return (
+
+    //const [data, setData] = useState({location: "hello!"});
+    const [filteredData, setFiltered] = useState(data);
+    const getEvents = async () => {
+      try{
+        const querySnapshot = await getDocs(collection(db, "event"));
+        let snapshot = []
+        querySnapshot.forEach((doc) => {
+          console.log(doc.data())
+          if (doc.data().category == category) {
+            snapshot[snapshot.length] = doc.data();
+          }
+        });
+        console.log(snapshot)
+        setFiltered(snapshot)
+      }
+      catch(error){
+          console.error(error);
+      }
+    }
+
+  useEffect(() => {
+      getEvents();
+  }, []);
+
+
+  return (
         <ScrollView style={{flex: 1, backgroundColor:"black"}}>
         <Text style={styles.title}>{category}</Text>
         <View style={styles.grid}>
-                {filteredData.map(((item, id) => {
+                {filteredData.map(((item) => {
                     // console.log(item);
-                    return (<EventChip key={id} eventData={item} navigation={navigation}></EventChip>)
+                    return (<EventChip eventData={item} navigation={navigation}></EventChip>)
                 }))}
         </View>
         </ScrollView>
     );
   }
   
-    const styles = StyleSheet.create({
+  const styles = StyleSheet.create({
     title: {
       color:"white", 
       fontWeight:'500',
