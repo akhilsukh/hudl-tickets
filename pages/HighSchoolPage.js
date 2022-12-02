55249da659ee450062ff06c05c7db3081d7ece94
Amal Deepak Thiru <adthiru@berkeley.edu>
2022-12-01 23:29:58 -0800

    Done filtering

--- pages/HighSchoolPage.js
+++ pages/HighSchoolPage.js
@@ -1,45 +1,59 @@
-import React from 'react'
+import React from 'react';
 import { StyleSheet, SafeAreaView, ScrollView, View, Text, Button, Image, requireNativeComponent } from 'react-native';
+import Banner from '../assets/banner.png';
+import Grid from '../components/Grid';
+import EventChip from '../components/EventChip';
+import ArchivedTickets from '../components/ArchivedTickets';
 import { db } from '../firebaseConfig.js';
-import { getDocs, collection, query, where} from 'firebase/firestore';
+import { getDocs, collection } from 'firebase/firestore';
 import { useState, useEffect } from 'react';
-import { EventChip } from '../components/EventChip'
+import data from '../components/data';
 
 export default function HighSchoolPage(props) {
   const [eventsData, setEvents] = useState([]);
-  const getData = async () => {
-    // const eventQuery = query(, where("capital", "==", true));
-    const events = query(collection(db, "cities"));
-    const snapshot = await getDocs(events)
-    setEvents(snapshot.data())
-    console.log(eventsData);
-  }
-  // const filter = () => {
-  //     /*let filtered = [];
-  //     eventsData.forEach((event) => {
-  //         if (event.awayTeam == props.away || event.homeTeam == props.name) {
-  //             filtered = [...filtered, event];
-  //         }
-  //     })
-  //     console.log(filtered);
-  //     await setSchoolEvents(filtered);*/
-  // }
+ 
 
-  useEffect(() => {
+  //console.log("PROPS PARAMS");
+  //console.log("-->>>>", JSON.stringify(props.route.params.name, null, 2));
+  // console.log("-->>>>", props.route.params);
+  
+  const getData = async () => {
+      const eventsRef = collection(db, "event");
+      const events = await getDocs(eventsRef);
+      if (events.size >= 0) {
+        let eventsList = [];
+        events.docs.forEach((doc) => {
+          let keyWords = doc.data().title.split(" ");
+          if (keyWords.includes(props.route.params.highSchool.name.split(" ")[0])) {
+            eventsList = [...eventsList, doc.data()];
+          }});
+       
+        await setEvents(eventsList);
+      }
+      else {
+        //console.log("doesnt exist")
+      }
+    };
+ 
+
+  useEffect( () => {
     getData();
-    console.log("EVENTS DATA", eventsData);
+  
   }, []);
 
   return (
-    <View>
-      <Text>{props.route.params.highSchool.name}</Text>
-      <Text>{props.route.params.highSchool.location}</Text>
-      {console.log(eventsData)}
+    <View style = {{flex : 1, color : "white", backgroundColor : "black"}}>
+      <Text style = {{color : "white", marginLeft : "32.5%"}} >{props.route.params.highSchool.name}</Text>
+      <Text style = {{color: "white", marginLeft : "32.5%"}}>{props.route.params.highSchool.location}</Text>
+      <Text style = {{color: "white", marginLeft : "32.5%"}}>Games:</Text>
+
+      
       {eventsData.map((event) => {
+       console.log("events" + eventsData);
         return (
-          <Text>{event.title}</Text>
+          <EventChip style = {{color: "white"}} eventData = {event} navigation = {props.route.params.navigation}></EventChip>
         )
-    })} 
+    })}
     </View>
   )
 }
